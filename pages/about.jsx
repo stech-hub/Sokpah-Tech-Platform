d6c0a657a0000fb8c04503a6fad0aaafe2135fb9
Akin Saye Sokpah <assokpah@gmail.com>
2026-01-16 02:46:05 +0000

    Create about.jsx

---
+++ pages/about.jsx
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <main className="page">
+      <h1>About Me</h1>
+      <p>
+        My name is Akin S. Sokpah, a Liberian tech founder and developer
+        passionate about building real-world digital solutions.
+      </p>
+    </main>
+  );
+}
