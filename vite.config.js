70ccfff000ea5ccb03352807c65b18fc4af57753
Akin Saye Sokpah <assokpah@gmail.com>
2026-01-16 02:12:57 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/", // ROOT because stech-hub.github.io
   plugins: [react()],
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-  },
 });
