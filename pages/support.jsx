2f48561ef14bf9f5080de8d44a7f2438fb2f99fc
Akin Saye Sokpah <assokpah@gmail.com>
2026-01-16 02:47:27 +0000

    Create support.jsx

---
+++ pages/support.jsx
@@ -0,0 +1,16 @@
+export default function Support() {
+  return (
+    <main className="page">
+      <h1>Support Center</h1>
+      <p>Need help or want to start a project?</p>
+
+      <a
+        href="https://wa.me/231777789356"
+        className="btn-primary"
+        target="_blank"
+      >
+        Contact Support on WhatsApp
+      </a>
+    </main>
+  );
+}
